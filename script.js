01858ad7e35e15cbb15a3f7edab21f0f140db72a
skykchmin <skykchmin@naver.com>
2020-12-15 02:51:23 +0900

    form-validator
    form-validator

--- script.js
+++ script.js
@@ -8,17 +8,73 @@ const repassword = document.getElementById('repassword');
 function showError(input, message){
     const formControl = input.parentElement;
     formControl.className = 'form-control error'; //className을 바꿈으로서 css에서 hidden -> visible 
-    const small = formControl.quertSelector('small');
+    const small = formControl.querySelector('small');
     small.innerText = message;
+}
+
+function showSuccess(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+//Check email is valid
+function checkEmail(email){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(input.value.trim())){
+        showSuccess(input);
+    }else{
+        showError(input, 'Email is not valid');
+    }
+}
+
+// Check required fields
+function checkRequired(inputArr){
+    inputArr.forEach(function(input) {
+        if (input.value.trim() === '') {
+          showError(input, `${getFieldName(input)} is required`);
+        } else {
+          showSuccess(input);
+        }
+      });
+}
+
+// Check input length
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(
+          input,
+          `${getFieldName(input)} must be at least ${min} characters`
+        );
+    } else if (input.value.length > max) {
+        showError(
+            input,
+            `${getFieldName(input)} must be less than ${max} characters`
+        );
+    } else {
+        showSuccess(input);
+    }
+}
+
+//
+function checkPasswordsMatch(input1, input2){
+    if(input1.value !== input2.value){
+        showError(input2, 'Passwords do not match');
+    }
+}
+
+// Get fieldname
+function getFieldName(input){
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 // Event listeners
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    if(username.value === ''){
-        showError(username, '이름을 입력해주세요');
-    } else {
-        showSuccess(username);
-    }
+    checkRequired([username, email, password, repassword]);
+    checkLength(username, 6, 15);
+    checkLength(password, 8, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, repassword);
 });
+
